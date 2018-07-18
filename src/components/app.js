a6893b3c2899342d55498f07edfeaa5ba08c7676
Michal <michalplebanski@gmail.com>
2018-07-18 16:54:46 +0200

    fixing API and adding map function

--- src/components/app.js
+++ src/components/app.js
@@ -23,19 +23,55 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<form onSubmit={event => this.onSubmit(event)}>
-					<label htmlFor="searchText">Search by user name</label>
-					<input
-						type="text"
-						placeholder="enter the user name"
-						id="searchText"
-						onChange={event => this.onChangeHandle(event)}
-						value={this.state.searchText}
-					/>
-				</form>
-				<div>{JSON.stringify(this.state.country)}</div>
-			</div>
+			<main>
+				<section class="search">
+					<form onSubmit={event => this.onSubmit(event)}>
+						<h2>Country search engine</h2>
+						<input
+							id="country-name"
+							placeholder="e.g. Poland"
+							type="text"
+							onChange={event => this.onChangeHandle(event)}
+							value={this.state.searchText}
+						/>
+					</form>
+				</section>
+				<section class="results">
+					<div class="country-info">
+						{this.state.country.map(item => (
+							<div class="response">
+								<h2 class="title-country">{item.name}</h2>
+								<div class="title-info">
+									Background Information:
+								</div>
+								<div class="info">
+									<ul class="list-title">
+										<li>Region</li>
+										<li>Alt spelling</li>
+										<li>Capital</li>
+										<li>Land area</li>
+										<li>Population</li>
+										<li>Language(s)</li>
+										<li>Currency</li>
+									</ul>
+									<ul class="list-value">
+										<li>{item.region}</li>
+										<li>{item.altSpellings[1]}</li>
+										<li>{item.capital}</li>
+										<li>{item.area}km</li>
+										<li>{item.population}</li>
+										<li>{item.languages}</li>
+										<li>{item.currencies}</li>
+									</ul>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<footer>
+					<p>Michał Plebański</p>
+				</footer>
+			</main>
 		);
 	}
 }
